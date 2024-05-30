9976ba8fdd989d8711554c189bb27e181ecbb0c2
Alex03198 <aleksandrsubbota0@gmail.com>
2024-05-31 02:03:44 +1000

    Add files via upload

---
+++ resolve2.py
@@ -0,0 +1,9 @@
+first=1
+second=2
+
+summa=first+second
+diff=first-second
+print(summa,";", diff)
+
+print(summa)
+print(diff)
---
+++ resolve3.py
@@ -0,0 +1,10 @@
+first=7
+second=14
+third=33
+
+numbers=[first,second,third]
+mean=average=sum(numbers)/len(numbers)
+print(mean)
+
+mean1=((first+second+third)/3)
+print(mean1)
